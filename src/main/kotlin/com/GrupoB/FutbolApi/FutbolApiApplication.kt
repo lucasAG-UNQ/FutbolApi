1479b54a88ad0cbc7b6d4bdfd7fdb8266eddf266
lucasAG-UNQ <lucasss162@hotmail.com>
2025-08-30 23:09:05 -0300

    initial commit

---
+++ src/main/kotlin/com/GrupoB/FutbolApi/FutbolApiApplication.kt
@@ -0,0 +1,11 @@
+package com.GrupoB.FutbolApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FutbolApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FutbolApiApplication>(*args)
+}
